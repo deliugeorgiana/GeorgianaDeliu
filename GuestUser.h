253737fecdb9c53fba616e5fc4149cdca5c0f010
Georgiana Deliu <deliugeorgiana11@gmail.com>
2024-12-05 23:41:49 +0200

    Update

--- GuestUser.h
+++ GuestUser.h
@@ -5,20 +5,14 @@
 
 class GuestUser : public User {
 public:
-    // Constructor cu validare prin moștenire
     explicit GuestUser(const std::string& name, const std::string& preferredLanguage)
             : User(name, preferredLanguage) {}
 
-    // Destructor virtual
     ~GuestUser() override = default;
 
     void DisplayProfile() const override;
 
     [[nodiscard]] std::shared_ptr<User> Clone() const override;
-
-    // Getter pentru nume (suprascriere)
-    [[nodiscard]] const std::string& GetName() const override { return name; }
-
 };
 
-#endif
+#endif // GUESTUSER_H
