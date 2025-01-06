8e91d3b61637898d697925d25fa46c60966efafe
Georgiana Deliu <deliugeorgiana11@gmail.com>
2025-01-06 13:09:48 +0200

    Update

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 cppcheck --enable=all \
-    --inline-suppr --max-configs=1000\
-    --project="${BUILD_DIR:-build}"/compile_commands.json \
+    --inline-suppr --max-configs=1000 \
+    --project="${BUILD_DIR:-build}/compile_commands.json" \
     -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
     -i"${EXT_DIR:-ext}" --suppress="*:${EXT_DIR:-ext}/*" \
     -i"${GEN_DIR:-generated}" --suppress="*:${GEN_DIR:-generated}/*" \
     --check-level=exhaustive \
-    --suppress=missingIncludeSystem\
+    --suppress=missingIncludeSystem \
     --suppress=unmatchedSuppression \
     --suppress=useStlAlgorithm \
     --error-exitcode=1
